8eb97b5d4b67079f8e9b905f0d26383e47d0927a
fmeulen <f.h.vandermeulen@tudelft.nl>
2021-09-20 13:27:54 +0200

    rounding example

---
+++ scr/rounding_example/rounding_example.jl
@@ -0,0 +1,37 @@
+using Plots
+using Distributions
+
+θ = 4.5
+n = 10000
+x = rand(Uniform(0,θ), n)
+y = map(x -> (x<1 ? x : Int(ceil(x))), x)
+
+histogram(y, label="observations"; normalize=true)
+
+mean(y.<1)
+mean(y.==2)
+
+lik <- function(th, x)
+{
+  if (x<min(1,th))
+  {
+    out <- 1
+  }  else {
+        
+  }
+  out/th
+}
+
+expec = function(θ)
+    out = 0.5θ
+    if θ > 1.0    
+        θc = ceil(θ)
+        θf = floor(θ)
+        out = (0.5+0.5*(θc-1.0)*θc-1.0+(θ-θf)*θc)/θ
+    end
+    out
+end
+
+θg = range(0.01, 3.5, length=100)
+plot(θg, expec.(θg), label="")
+
